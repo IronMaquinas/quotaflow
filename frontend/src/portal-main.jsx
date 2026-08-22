3fbf201e471042da355087d7bf1fc6be9ca5c780
IronMaquinas <ironbetaeyes@gmail.com>
2026-08-21 23:08:20 -0300

    Deploy production setup - add env files and portal

--- frontend/src/portal-main.jsx
+++ frontend/src/portal-main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TelaPortalFornecedor from './components/portal/TelaPortalFornecedor'
-import './index.css' // Se tiver
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
